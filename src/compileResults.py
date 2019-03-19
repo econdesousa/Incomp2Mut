c65b6e91e6233ee5d7a105e8d0ba0d2ff1ff7353
econdesousa <econdesousa@gmail.com>
2019-03-19 13:38:44 +0000

    almost finished (plots not yet)

--- src/compileResults.py
+++ src/compileResults.py
@@ -2,11 +2,15 @@ import tkinter as tk
 from tkinter import filedialog
 import os
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+
 
 root = tk.Tk()
 root.withdraw()
 WorkingDir = filedialog.askdirectory(initialdir=os.path.join(os.getcwd(), "Markers"), title='Please select a directory')
-
 
 def getDirNames():
     dirList = []
@@ -74,40 +78,57 @@ def ReadVecData(f):
 
 
 def exportReport(outFileName, inFileName, d2F, d2M):
+    #outFileName: path to file to be edited
+    #inFileName: name to append to data
+    #d2M and d2F data to use in computations
     print(outFileName)
-    # PossibleCases = { "trios" : 1, "Duos Father": 2, "Duos Mother": 3}
+    # sort data to avoid having e.g. [0,1] and [1,0] that is equivalent -> 1 mutation step
     for i in range(len(d2F)):
         if d2M[i] < d2F[i]:
             tmpvar = d2M[i]
             d2M[i] = d2F[i]
             d2F[i] = tmpvar
-
+    #count unique rows of matrix [d2F,d2M]
     matrixOut = np.array([[float(x) for x in d2F], [float(x) for x in d2M]])
     matrixOut = np.transpose(matrixOut)
     [unique_rows, cnt] = np.unique(matrixOut, axis=0, return_counts=True)
 
+    tmp=inFileName.split(sep=".txt")
+    inFileName=tmp[0]
+    fid = open(outFileName,'w')
+    index=[]
     for i in range(len(cnt)):
-        print(inFileName, "\t", unique_rows[i, 0], "\t", unique_rows[i, 1], "\t", cnt[i], file=f)
+        index.append('['+str(unique_rows[i, 0])+' , '+str(unique_rows[i, 1])+']')
+        print(inFileName, "\t", unique_rows[i, 0], "\t", unique_rows[i, 1], "\t", cnt[i], file=fid)
         inFileName = ""
+
+    df = pd.DataFrame({'cnt':cnt},index=index)
+    ax = df.plot.bar(rot=0)
+    outDir = os.path.dirname(outFileName)
+    with PdfPages(os.path.join(outDir,tmp[0]+'.pdf')) as pdf:
+        pdf.savefig()
+
+    fid.close()
 
 
 def main(WorkingDir, resultFolderName="compiledResults"):
     finalList = getFileNames()
-    print("finalList =", finalList)
-    print("\n\n", finalList[0])
     pDir = os.path.abspath(os.path.join(WorkingDir, os.pardir))
     pDir = os.path.join(pDir, resultFolderName);
     print("outDIR = ", pDir)
     if not os.path.exists(pDir):
         os.makedirs(pDir)
-
-    f = open(outFileName, 'w')
-    f.close()
+    else:
+        files = [f for f in os.listdir(pDir) if os.path.isfile(os.path.join(pDir,f))]
+        for f in files:
+            print(os.path.join(pDir, f))
+            os.remove(os.path.join(pDir, f))
 
     for f in finalList:
         tmp = f.split(sep=os.sep)
         inDir = os.path.dirname(f)
         outFileName = os.path.join(pDir, tmp[-2] + ".txt")
+
         d2F, d2M, d2Fset, d2Mset = ReadVecData(f)
         # PossibleCases = { "trios" : 1, "Duos Father": 2, "Duos Mother": 3}
         # WorkingCase = getWorkingCase(d2Fset,d2Mset)
